0845671d93ef493bd17301d8cb0e4d24472bc842
ehliesho <ehliesho@avans.nl>
2018-10-10 11:21:27 +0200

    no message

--- app/src/main/java/com/example/johanna/runis/BatteryPowerNotifier.kt
+++ app/src/main/java/com/example/johanna/runis/BatteryPowerNotifier.kt
@@ -1,7 +1,21 @@
-package com.example.johanna.runis.Settings
+package com.example.johanna.runis
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import android.os.BatteryManager
+
+
 
 /**
  * Created by Edward on 10.10.2018.
  */
-class BatteryPowerNotifier {
+class BatteryPowerNotifier : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val curLevel = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+        if(curLevel == 99){
+            Toast.makeText(context, "Battery is running low!", Toast.LENGTH_LONG).show()
+        }
+    }
 }
